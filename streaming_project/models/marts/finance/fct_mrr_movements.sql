e790a11af0d5fd92f306b7fb609f2f5b4c44e6fa
Joabe Santos <jbencao37@gmail.com>
2026-09-09 16:22:34 -0300

    fix: make dbt CI reproducible on clean environments

--- streaming_project/models/marts/finance/fct_mrr_movements.sql
+++ streaming_project/models/marts/finance/fct_mrr_movements.sql
@@ -4,7 +4,7 @@ SELECT
   movement_id,
   subscription_id,
   customer_id,
-  movement_month,
+  CAST(movement_month AS DATE) AS movement_month,
   movement_type,
   CAST(mrr_delta_usd AS DECIMAL(18, 2)) AS mrr_delta_usd
 FROM {{ ref('int_mrr_movements') }}
